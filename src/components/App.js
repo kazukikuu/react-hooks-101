9aa327b2ceea551e2941cb12244351b958826897
kuukazuki <16ikuta@seian.jp>
2020-07-23 16:46:35 +0900

    replace type names with constants

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,21 @@
-import React, { useReducer, useState } from 'react';
+import React, { useReducer } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import reducer from '../reducers'
+
+import Events from './Events'
+import EventForm from './EventForm'
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
-  const addEvent = e => {
-    e.preventDefault()
-    dispatch({
-      type: 'CREATE_EVENT',
-      title,
-      body
-    })
-    setTitle('')
-    setBody('')
-  }
-
-  console.log({ state })
   return (
     <div className="container-fluid">
-      <h4>イベント作成フォーム</h4>
-      <form>
-        <div className="form-group">
-          <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)} />
-        </div>
 
-        <div className="form-group">
-          <label htmlFor="formEventBody">ボディー</label>
-          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
-        </div>
 
-        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-        <button className="btn btn-danger">全てのイベントを消去する</button>
-      </form>
+      <EventForm state={state} dispatch={dispatch} />
+      <Events state={state} dispatch={dispatch} />
 
-      <h4>イベント一覧</h4>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>ボディー</th>
-            <th></th>
-          </tr>
-        </thead>
-      </table>
-    </div>
+    </div >
   );
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,11 @@
-// action = {
-//   type: 'CREATE_EVENT',
-//   title: '東京オリンピックのお知らせ',
-//   body: '開催します！つきましては、、'
-// }
-// state = []
-// state = [
-//   { id: 1, title: 'タイトル', body: 'ボディー1' }
-//     { id: 2, title: 'タイトル', body: 'ボディー2' }
-//       { id: 3, title: 'タイトル', body: 'ボディー3' }
-// ]
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  DELETE_EVENT
+} from '../actions'
 const events = (state = [], action) => {
   switch (action.type) {
-    case 'CREATE_EVENT':
+    case CREATE_EVENT:
       const event = { title: action.title, body: action.body }
       const length = state.length
       let id
@@ -21,9 +15,10 @@ const events = (state = [], action) => {
         id = state[length - 1].id + 1
       }
       return [...state, { id, ...event }]
-    case 'CREATE_EVENT':
-      return state
-    case 'DELETE_ALL_EVENTS':
+    case DELETE_EVENT:
+      return state.filter(event => event.id !== action.id)
+    case DELETE_ALL_EVENTS:
+
       return []
     default:
       return state
