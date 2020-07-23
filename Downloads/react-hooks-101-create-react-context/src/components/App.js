e077f62d3b93333292f793ca979cf731ad8385a5
kuukazuki <16ikuta@seian.jp>
2020-07-24 01:47:11 +0900

    solve prop drilling issue

---
+++ Downloads/react-hooks-101-create-react-context/src/components/App.js
@@ -0,0 +1,23 @@
+import React, { useReducer } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import AppContext from '../contexts/AppContext'
+import Events from './Events'
+import EventForm from './EventForm'
+import reducer from '../reducers'
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+
+      <div className="container-fluid">
+        <EventForm />
+        <Events />
+      </div >
+    </AppContext.Provider>
+  );
+}
+
+export default App;
