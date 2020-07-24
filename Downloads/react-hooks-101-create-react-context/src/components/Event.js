db515439dc6fde4fbdf354b95618874f30f7a41f
kuukazuki <16ikuta@seian.jp>
2020-07-24 16:22:36 +0900

    create operation log in case of event deletion

--- Downloads/react-hooks-101-create-react-context/src/components/Event.js
+++ Downloads/react-hooks-101-create-react-context/src/components/Event.js
@@ -1,7 +1,12 @@
 import React, { useContext } from 'react'
 
-import { DELETE_EVENT } from '../actions'
+import {
+  ADD_OPERATION_LOG,
+  DELETE_EVENT
+} from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
+
 
 const Event = ({ event }) => {
   const { dispatch } = useContext(AppContext)
@@ -10,6 +15,12 @@ const Event = ({ event }) => {
     const result = window.confirm(`イベントID: ${id}イベントを本当に削除してもよろしいですか`)
 
     dispatch({ type: DELETE_EVENT, id })
+
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: `イベント(id=${id})を削除しました。`,
+      operatedAt: timeCurrentIso8601()
+    })
   }
 
   return (
